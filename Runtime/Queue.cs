d529a582ebbca5da61e57e02eaa42116c5eca7b6
Jean-Denis "JD" Boivin <jd@boiv.in>
2025-03-26 16:31:58 -0400

    Easier usage when replacing "using"

---
+++ Runtime/Queue.cs
@@ -0,0 +1,9 @@
+﻿namespace JD.DS
+{
+    // Already alloc free
+    public class Queue<T> : System.Collections.Generic.Queue<T>
+    {
+        public Queue() : base() { }
+        public Queue(int capacity) : base(capacity) { }
+    }
+}
---
+++ Runtime/Stack.cs
@@ -0,0 +1,9 @@
+﻿namespace JD.DS
+{
+    // Already alloc free
+    public class Stack<T> : System.Collections.Generic.Stack<T>
+    {
+        public Stack() : base() { }
+        public Stack(int capacity) : base(capacity) { }
+    }
+}
