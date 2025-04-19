0b4e8e7a2200d17a15b95d6e8f817a4797363260
Jean-Denis "JD" Boivin <jd@boiv.in>
2025-04-19 18:14:40 -0400

    Basic `Pool`

---
+++ Runtime/Pool.cs
@@ -0,0 +1,36 @@
+﻿namespace JD.DS
+{
+    public interface IPoolable<T>
+    {
+        //public T Get();
+        public T Create();
+        //public void Release(T bag);
+    }
+    
+    public class Pool<T> where T : class
+    {
+        private readonly IPoolable<T> _poolable;
+        private readonly Queue<T> _pool;
+
+        public Pool(IPoolable<T> poolable, int capacity = 100, bool fill = true)
+        {
+            _poolable = poolable;
+            _pool = new Queue<T>(capacity);
+            if (fill) for (var i = 0; i < capacity; i++)
+                _pool.Enqueue(poolable.Create());
+        }
+        
+        public T Get()
+        {
+            if (_pool.TryDequeue(out var result))
+                return result;
+            
+            return _poolable?.Create();
+        }
+
+        public void Release(T item)
+        {
+            _pool.Enqueue(item);
+        }
+    }
+}
---
+++ Runtime/PoolGameObject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace JD.DS
+{
+    public class PoolGameObject<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public T Prefab;
+        
+        [NonSerialized] public List<T> List;
+        private Queue<T> _pool;
+
+        public int Capacity = 100;
+        public bool Fill = true;
+        
+        private void Awake()
+        {
+            List = new List<T>(Capacity);
+            _pool = new Queue<T>(Capacity);
+            if (Fill) for (var i = 0; i < Capacity; i++)
+                _pool.Enqueue(Create());
+        }
+
+        public T Get()
+        {
+            if (!_pool.TryDequeue(out var result))
+                result = Create();
+
+            List.Add(result);
+            return result;
+        }
+        
+        public T Create()
+        {
+            return Instantiate(Prefab, transform);
+        }
+        
+        public void Release(T component)
+        {
+            // Moving very far is less costly than disabling
+            // Obviously, we need to not use `Update()`, etc.
+            // TODO: Profile again to makes sure
+            var obj = component.gameObject;
+            obj.transform.localPosition = new Vector3(-90000, -90000, -90000);
+            
+            //obj.SetActive(false);
+            
+            _pool.Enqueue(component);
+            List.Remove(component);
+        }
+    }
+}
